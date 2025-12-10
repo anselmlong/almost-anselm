14ff31b00b353e09df4793a0ca64edd283734cc6
Anselm Long <anselmpius@gmail.com>
2025-12-10 16:46:41 -0600

    Add vllm script

--- src/inference/check_vllm.py
+++ src/inference/check_vllm.py
@@ -2,10 +2,11 @@ from vllm import LLM, SamplingParams
 prompts = [
     "Mexico is famous for ",
     "The largest country in the world is ",
+    "what's up anselm"
 ]
 
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
-llm = LLM(model="facebook/opt-125m")
+llm = LLM(model="anselmlong/almost-anselm")
 responses = llm.generate(prompts, sampling_params)
 
 for response in responses:
